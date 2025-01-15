2640b18b659fc16ce912a0297dbebbc31ec991e6
Vermont Garcia <vermontgarcia@gmail.com>
2025-01-15 12:35:51 -0700

    Excercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine("");
     }
 }
