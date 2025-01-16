aa3606df64ae00ef219022c49c61d1c4ef63bf8c
Vermont Garcia <vermontgarcia@gmail.com>
2025-01-15 17:10:32 -0700

    Excercise 3 Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessedTimes = 0;
+            do
+            {
+                Console.Write("Enter your guess: ");
+                // Handle exception when user not provide a valid numerical input
+                try
+                {
+                    guess = int.Parse(Console.ReadLine());
+                    guessedTimes += 1;
+                    if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it! It took you {guessedTimes} times!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain.ToLower() == "yes");
     }
 }
