4bae4ff1c4868ada6d95f8ea653abead7b816673
Vermont Garcia <vermontgarcia@gmail.com>
2025-01-20 01:29:15 -0700

    Program Class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Instantiating Job instances (Curremt Employment no endYear)
+        Job job1 = new Job("Microsoft", "Sr. Software Engineer", 2020);
+        Job job2 = new Job("Amazon", "Software Engineer", 2013, 2020);
+
+        // Instantiating Resume instance with name parameter only and then adding jobs with AddJob method 
+        Resume resume = new Resume("Vermont Garcia");
+        resume.AddJob(job1);
+        resume.AddJob(job2);
+        resume.Display();
+        Console.WriteLine("");
+
+        // Instantiating Resume instance with name and jobs parameters
+        Resume resume2 = new Resume("John Dow", [job1, job2]);
+        resume2.Display();
+        Console.WriteLine("");
+
+        // Instatiating Resume instance with only name parameter Display method shows "No jobs to show"
+        Resume resume3 = new Resume("Iris Gerber");
+        resume3.Display();
     }
 }
