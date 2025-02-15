4d966c3e2742bcd36a341ee657ef04f41bccd2a1
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-15 11:17:18 -0700

    Customer class

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,33 @@
+public class Customer
+{
+  private string _fName;
+  private string _lName;
+  private Address _address;
+
+  public Customer(string fName, string lName)
+  {
+    _fName = fName;
+    _lName = lName;
+  }
+  public Customer(string fName, string lName, Address address)
+  {
+    _fName = fName;
+    _lName = lName;
+    _address = address;
+  }
+
+  public void SetAddress(Address address)
+  {
+    _address = address;
+  }
+
+  public bool IsUSALocated()
+  {
+    return _address.IsDomestic();
+  }
+
+  public string GetFullName()
+  {
+    return $"{_fName} {_lName}";
+  }
+}
