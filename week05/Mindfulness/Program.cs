c99c693324344359668047cb0b22e029de0c5661
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-17 14:05:20 -0700

    Menu

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.Clear();
+        Console.WriteLine("Welcome to Mindfulness Project.");
+
+        int choice = 0;
+
+        do
+        {
+            try
+            {
+                DisplayMenu();
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                choice = 0;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Breathing Activity");
+                    break;
+                case 2:
+                    Console.WriteLine("Listing Activity");
+                    break;
+                case 3:
+                    Console.WriteLine("Reflecting Activity");
+                    break;
+                case 9:
+                    Console.WriteLine("Goodbye, comeback soon!");
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid option");
+                    break;
+            }
+        } while (choice != 9);
     }
+
+    // Function to display menu
+    static void DisplayMenu()
+    {
+
+        string menu = @"
+1. Breathing Activity.
+2. Listing Activity.
+3. Reflecting Activity.
+
+9. Quit.
+";
+
+        Console.WriteLine(menu);
+        Console.Write("What would you like to do? ");
+    }
+
+    // Function to get user input
+    static string GetUserEntry(string prompt)
+    {
+        Console.Write($"{prompt} ");
+        string userInput = Console.ReadLine();
+        return userInput;
+    }
+
 }
