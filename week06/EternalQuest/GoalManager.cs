f6ded0dabc0ab5b2fb085515c52a38cea8f3e7ca
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-19 05:33:50 -0700

    Goal Manager Methods Update

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -3,33 +3,55 @@ using System.Text.Json;
 public class GoalManager
 {
   private List<Goal> _goals;
-  private int _score;
+  // private int _score;
 
   public GoalManager()
   {
     _goals = [];
-    _score = 0;
+    // _score = 0;
   }
 
   public void Start()
   {
+    LoadGoals();
+    ListGoalDetails();
+    DisplayPlayerInfo();
+    ListGoalNames();
+    // _goals[1].RecordEvent();
+    DisplayPlayerInfo();
+    ListGoalDetails();
+    SaveGoals();
+
   }
 
   public void DisplayPlayerInfo()
   {
+    int totalPoints = _goals.Sum(goal => goal.EarnedPoints);
+    Console.WriteLine($"Score {totalPoints}");
   }
 
   public void ListGoalNames()
   {
+    if (_goals.Count > 0)
+    {
+      foreach (int i in Enumerable.Range(0, _goals.Count))
+      {
+        Console.WriteLine($"{i + 1}. {_goals[i].Name}");
+      }
+    }
+    else
+    {
+      Console.WriteLine("No goals to display...");
+    }
   }
 
   public void ListGoalDetails()
   {
     if (_goals.Count > 0)
     {
-      foreach (Goal goal in _goals)
+      foreach (int i in Enumerable.Range(0, _goals.Count))
       {
-        Console.WriteLine(goal.GetDetailsString());
+        Console.WriteLine($"{i + 1}. {_goals[i].GetDetailsString()}");
       }
     }
     else
@@ -53,18 +75,33 @@ public class GoalManager
   {
   }
 
-  public void SaveGoals(string fileName)
+  public void SaveGoals(string fileName = "goalsBackupAppFile.json")
   {
     var options = new JsonSerializerOptions { Converters = { new GoalConverter() }, WriteIndented = true };
     string jsonString = JsonSerializer.Serialize(_goals, options);
     File.WriteAllText(fileName, jsonString);
   }
 
-  public void LoadGoals(string fileName)
+  public void LoadGoals(string fileName = "goalsBackupAppFile.json")
   {
-    string jsonString = File.ReadAllText(fileName);
-    var options = new JsonSerializerOptions { Converters = { new GoalConverter() }, WriteIndented = true };
-    List<Goal> deserializedGoals = JsonSerializer.Deserialize<List<Goal>>(jsonString, options);
-    _goals = deserializedGoals;
+    try
+    {
+      string jsonString = File.ReadAllText(fileName);
+      var options = new JsonSerializerOptions { Converters = { new GoalConverter() }, WriteIndented = true };
+      List<Goal> deserializedGoals = JsonSerializer.Deserialize<List<Goal>>(jsonString, options);
+      _goals = deserializedGoals;
+
+    }
+    catch (FileNotFoundException ex)
+    {
+      if (fileName != "goalsBackupAppFile.json")
+      {
+        Console.WriteLine($"Error: File not found - {ex.FileName}");
+      }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"General error: {ex.Message}");
+    }
   }
 }
