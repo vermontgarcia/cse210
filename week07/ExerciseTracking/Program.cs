8959d8649811665a8e99bcce1d08069698c374f6
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-18 21:39:08 -0700

    Testing Classes in main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("ExerciseTracking Project.");
+
+        Running running = new(30, 5);
+
+        Console.WriteLine(running.GetDistance());
+        Console.WriteLine(running.GetSpeed());
+        Console.WriteLine(running.GetPace());
+        Console.WriteLine();
+        Console.WriteLine(running.GetSummary());
+        Console.WriteLine();
+
+        Cycling cycling = new(23, 18.3);
+
+        Console.WriteLine(cycling.GetDistance());
+        Console.WriteLine(cycling.GetSpeed());
+        Console.WriteLine(cycling.GetPace());
+        Console.WriteLine();
+        Console.WriteLine(cycling.GetSummary());
+        Console.WriteLine();
+
+        Swimming swimming = new(50, 42);
+
+        Console.WriteLine(swimming.GetDistance());
+        Console.WriteLine(swimming.GetSpeed());
+        Console.WriteLine(swimming.GetPace());
+        Console.WriteLine();
+        Console.WriteLine(swimming.GetSummary());
+        Console.WriteLine();
+
     }
 }
