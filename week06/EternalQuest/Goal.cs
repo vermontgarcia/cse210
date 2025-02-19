cb69ae5751396a8ad617770b166d1083453f35be
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-19 03:39:42 -0700

    Base abstract Goal Class

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,53 @@
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+public abstract class Goal
+{
+  private string _shortName;
+  private string _description;
+  private int _points;
+
+  public Goal(string name, string description, int points)
+  {
+    TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+    string titleCase = textInfo.ToTitleCase(name);
+    _shortName = titleCase;
+    _description = description;
+    _points = points;
+  }
+
+  public string Name
+  {
+    get { return _shortName; }
+    set
+    {
+      TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+      string titleCase = textInfo.ToTitleCase(value);
+      _shortName = titleCase;
+    }
+  }
+
+  public string Description
+  {
+    get { return _description; }
+    set { _description = value; }
+  }
+
+  public int Points
+  {
+    get { return _points; }
+    set { _points = value; }
+  }
+
+  public abstract void RecordEvent();
+
+  public abstract bool IsCompleted();
+
+  public abstract string GetStringRepresentation();
+
+  public virtual string GetDetailsString()
+  {
+    string isCompleted = IsCompleted() ? "X" : " ";
+    return $"[{isCompleted}] {_shortName} ({_description})";
+  }
+}
