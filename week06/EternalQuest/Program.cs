72fcfcc581f33fa2a45781781b7ccd5b7813c6a7
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-19 03:47:28 -0700

    Main Program Partial implementation

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        DisplayWelcome();
+
+        GoalManager goalManager = new();
+        goalManager.Start();
+
+        // // goalManager.SaveGoals("goals.json");
+
+        // goalManager.LoadGoals("goals.json");
+        // goalManager.ListGoalDetails();
+    }
+
+    static void DisplayWelcome()
+    {
+        ClearScreen();
+        Console.WriteLine("Welcome to EternalQuest App!");
+        Console.WriteLine();
+    }
+
+    static void ClearScreen()
+    {
+        Console.Clear();
     }
 }
