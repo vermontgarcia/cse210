31ae0d53e1f771c883f4162391c0b5985238a315
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-18 21:53:12 -0700

    Base program List

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,34 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         Console.WriteLine("ExerciseTracking Project.");
 
+        Console.WriteLine();
         Running running = new(30, 5);
-
-        Console.WriteLine(running.GetDistance());
-        Console.WriteLine(running.GetSpeed());
-        Console.WriteLine(running.GetPace());
-        Console.WriteLine();
-        Console.WriteLine(running.GetSummary());
-        Console.WriteLine();
-
         Cycling cycling = new(23, 18.3);
-
-        Console.WriteLine(cycling.GetDistance());
-        Console.WriteLine(cycling.GetSpeed());
-        Console.WriteLine(cycling.GetPace());
-        Console.WriteLine();
-        Console.WriteLine(cycling.GetSummary());
-        Console.WriteLine();
-
         Swimming swimming = new(50, 42);
 
-        Console.WriteLine(swimming.GetDistance());
-        Console.WriteLine(swimming.GetSpeed());
-        Console.WriteLine(swimming.GetPace());
-        Console.WriteLine();
-        Console.WriteLine(swimming.GetSummary());
-        Console.WriteLine();
+        List<Activity> activities = [running, cycling, swimming];
 
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+        Console.WriteLine();
     }
 }
